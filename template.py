6420fd2d654a4d1f0aa5b43cb09c8d7e45c09290
valldun1 <vall.dun420@gmail.com>
2026-03-31 11:07:37 +0000

    Update: Reviews + request form + Lucia40 content

--- template.py
+++ template.py
@@ -8,8 +8,8 @@ import os, re
 
 TG_BOT = "https://t.me/captainatlanticbot"
 SITE = "https://atlanticsail.net"
-WA = "https://wa.me/+34605675361"
-PHONE1 = "+34 605-675-361"
+WA = "https://wa.me/34603192089?text=%D0%A5%D0%BE%D1%87%D1%83%20%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C%20%D0%BE%20%D0%BF%D1%83%D1%82%D0%B5%D1%88%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D0%B8"
+PHONE1 = "+34 603-192-089"
 PHONE2 = "+7(903)55-00-654"
 EMAIL = "info@atlanticsail.net"
 YT = "https://youtube.com/@capitankosta"
